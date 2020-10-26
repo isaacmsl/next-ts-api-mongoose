cf69924a7c4300fe73e74547a6128b7a3d499233
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-10-25 23:45:23 -0300

    ++mongooseClient cache 60s

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import mongooseClient from '../../../database/mongooseClient'
 import { NextApiRequest, NextApiResponse } from 'next'
 import nextConnect from 'next-connect'
 
@@ -8,14 +8,11 @@ const endpoint = nextConnect()
 
 endpoint.get(async (req: NextApiRequest, res: NextApiResponse) => {
     // em development, não use cache; em production, use cache e tente revalidar a cada 60s; deixe cache public nas CDNs da vercel
-    res.setHeader('Cache-Control', 'max-age=0, s-maxage=120, stale-while-revalidate, public')
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=60, stale-while-revalidate, public')
 
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
-        })
+        await mongooseClient.connect()
+        
         const users = await User.find()
 
         return res.status(200).json({ sucess: true, qnt: users.length, data: users })
