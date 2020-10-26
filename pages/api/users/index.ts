da1838743899998450427a9b22005d6b11090414
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2020-10-25 22:51:29 -0300

    --dbConnect 120s cache

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,18 +1,21 @@
+import mongoose from 'mongoose'
 import { NextApiRequest, NextApiResponse } from 'next'
 import nextConnect from 'next-connect'
-import dbConnect from '../../../database/dbConnect'
 
 import { User, IUserModel } from '../../../models/User'
 
 const endpoint = nextConnect()
 
-endpoint.use(dbConnect)
-
 endpoint.get(async (req: NextApiRequest, res: NextApiResponse) => {
     // em development, não use cache; em production, use cache e tente revalidar a cada 60s; deixe cache public nas CDNs da vercel
-    res.setHeader('Cache-Control', 'max-age=0, s-maxage=60, stale-while-revalidate, public')
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=120, stale-while-revalidate, public')
 
     try {
+        await mongoose.connect(process.env.MONGO_DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
         const users = await User.find()
 
         return res.status(200).json({ sucess: true, qnt: users.length, data: users })
